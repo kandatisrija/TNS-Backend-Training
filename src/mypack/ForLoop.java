47b91ae31163e2ecfc526ff21a754cc87e5791f2
SRIJA <SRIJA@HPPAVILION>
2025-11-04 20:30:21 +0530

    ForLoop

---
+++ src/mypack/ForLoop.java
@@ -0,0 +1,13 @@
+package mypack;
+
+public class ForLoop {
+
+    public static void main(String[] args) {
+
+        // ----- For Loop -----
+        System.out.println("For Loop:");
+        for (int i = 1; i <= 5; i++) {     // runs 5 times
+            System.out.println("Count: " + i);
+        }
+    }
+}
